e85411f1cf88296c4adfd3d3347a5919383cd683
sc6666666 <Chensiqi0426@gmail.com>
2025-11-16 20:58:40 -0500

    “test”

--- calculator.py
+++ calculator.py
@@ -7,20 +7,14 @@ One function per operation, in order.
 # First example
 import math
 
-def add(a, b):
-    return a + b
 
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
-    return a * b
 
 def add(a, b):
     return a + b
 
-def sub(a, b):
-    return a - b
 
 def mul(a, b):
     return a * b
@@ -30,25 +24,17 @@ def div(a, b):
         raise ZeroDivisionError
     return b / a
 
-def log(a, b):
-    if a <= 0 or a == 1:
-        raise ValueError
-    if b <= 0:
-        raise ValueError
-    return math.log(b, a)
 
 def exp(a, b):
     return math.pow(a, b)
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1:
         raise ValueError
     if b <= 0:
         raise ValueError
     return math.log(b, a)# use math library/raise ValueError
 
-def exponent(a, b):
-    return math.pow(a, b)
 
 def square_root(a):
     if a < 0:
@@ -57,5 +43,3 @@ def square_root(a):
 
 def hypotenuse(a, b):
     return math.hypot(a, b)
-
-#123
