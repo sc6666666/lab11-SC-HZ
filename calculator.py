455d005eefa354cc5fd0d0d61bf5307816b715c2
sc6666666 <Chensiqi0426@gmail.com>
2025-11-16 20:57:04 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -16,7 +16,29 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def add(a, b):
+    return a + b
 
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError
+    if b <= 0:
+        raise ValueError
+    return math.log(b, a)
+
+def exp(a, b):
+    return math.pow(a, b)
 
 def log(a, b):
     if a <= 0 or a == 1:
